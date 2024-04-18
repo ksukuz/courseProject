0643b6e01c47f90dbf179ad55befbac37ba8d04a
Ksenia Kuzmitskaya <90525001+ksukuz@users.noreply.github.com>
2024-04-19 00:42:36 +0300

    Update app.go

--- app.go
+++ app.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+func handler(w http.ResponseWriter, r *http.Request) 
+  if r.Method == "GET" 
+    w.Header().Set("Content-Type", "text/html")
+    fmt.Fprintf(w, "<form method='POST'>"+
+      "<label for='name'>What's your name?</label>"+
+      "<input type='text' id='name' name='name'>"+
+      "<button type='submit'>Submit</button>"+
+      "</form>")
+  } else if r.Method == "POST" {
+    r.ParseForm()
+    name := r.Form.Get("name")
+    fmt.Fprintf(w, "Hello, %s! Nice to meet you!\n", name)
+  }
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Running demo app. Press Ctrl+C to exit...")
-	log.Fatal(http.ListenAndServe(":8888", nil))
+  http.HandleFunc("/", handler)
+  fmt.Println("Running demo app. Press Ctrl+C to exit...")
+  log.Fatal(http.ListenAndServe(":8888", nil))
 }
+
