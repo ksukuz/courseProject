8f486e72c2194584ed773ff1bf3fd1001bb0834f
Ksenia Kuzmitskaya <90525001+ksukuz@users.noreply.github.com>
2024-04-19 11:01:54 +0300

    Update app.go

--- app.go
+++ app.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Fprintf(w, "Hello, 世界! The current server time is: %s", currentTime)
+	fmt.Fprintln(w, "Hello, 世界")
 }
 
 func main() {
